ad369f522d1bed562d074876f1cf841d12f341c2
reece-elder <reece.elder@qa.com>
2023-02-07 16:33:46 +0000

    RE: All basic functions working

--- Backend/order.py
+++ Backend/order.py
@@ -1,9 +1,9 @@
 class Order:
 
-    def __init__(self, order_id, item_id, quantity, cust_name, date_time):
+    def __init__(self, order_id, item_id, quantity, total_cost, date_time):
         self.order_id = order_id
         self.item_id = item_id
         self.quantity = quantity
-        self.cust_name = cust_name
+        self.total_cost = total_cost
         self.date_time = date_time
         
