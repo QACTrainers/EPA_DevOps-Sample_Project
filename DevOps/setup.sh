99c2487bc134bfb2bef450e919869f0819e993f9
reece-elder <reece.elder@qa.com>
2023-02-09 15:30:56 +0000

    RE: Setup frontend nginx + docker

--- DevOps/setup.sh
+++ DevOps/setup.sh
@@ -17,3 +17,13 @@ terraform -help
 $ sudo apt-add-repository --yes --update ppa:ansible/ansible
 $ sudo apt install ansible -y
 ansible --version
+
+# Docker Install
+sudo apt update
+sudo apt install curl -y
+curl https://get.docker.com | sudo bash
+
+sudo usermod -aG docker $(whoami)
+sudo reboot
+
+docker run --rm hello-world
