85a95b574cb4d30a94f184e95268259be7c3b6d8
reece-elder <reece.elder@qa.com>
2023-02-10 12:01:07 +0000

    changed dockerfile

--- DevOps/docker_setup.sh
+++ DevOps/docker_setup.sh
@@ -1,6 +1,8 @@
 docker build -t epa_frontend .
 docker build -t epa_backend .
 
+docker build . -t epa_frontend_img -f Backend/Dockerfile
+
 docker run -d --network new_network -p 80:80 --name frontend epa_frontend
 docker run -d --network new_network -p 5000:5000 --name backend epa_backend
 
