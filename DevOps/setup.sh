e01b7470a0ecd3e7b146522e3051c81d5778c18c
Reece Elder <71394624+Reece-elder@users.noreply.github.com>
2023-02-16 13:34:45 +0000

    Update setup.sh

--- DevOps/setup.sh
+++ DevOps/setup.sh
@@ -13,41 +13,12 @@ sudo apt-get update && sudo apt-get install terraform
 # Verify
 terraform -help
 
-# Ansible Install
-$ sudo apt-add-repository --yes --update ppa:ansible/ansible
-$ sudo apt install ansible -y
-ansible --version
-
-# Docker Install
-sudo apt update
-sudo apt install curl -y
-curl https://get.docker.com | sudo bash
-
-sudo usermod -aG docker $(whoami)
-sudo reboot
-
-docker run --rm hello-world
-
-# Docker Compose 
-# make sure jq & curl is installed
-sudo apt update
-sudo apt install -y curl jq
-# set which version to download (latest)
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-# download to /usr/local/bin/docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# make the file executable
-sudo chmod +x /usr/local/bin/docker-compose
-
-docker-compose --version
-
-
-# EKS
+# Install AWS CLI and set up kubectl
 
 sudo apt update
 sudo apt install awscli
+echo "enter access key, secret key, region eu-west-1 within aws configure on next command"
 aws configure 
-(enter access key, secret key, region eu-west-1)
 
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
 
@@ -58,7 +29,3 @@ mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
 kubectl version --short --client
-
-aws eks update-kubeconfig --region eu-west-1 --name epa-cluster
-
-kubectl get svc
